a3507e4eb2dd54087b5d52f89572f078e179ecde
Your Name <you@example.com>
2026-01-17 13:59:06 +0530

    first-commit

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-chatbot-10-5xb3.onrender.com/api",
 });
 
 export const fetchSessionsAPI = () => API.get("/chat/sessions");
